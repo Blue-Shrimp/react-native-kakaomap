5f3bc6056117629f52752349d3dbef7305831d41
Blue-Shrimp <wkdtmd1202@naver.com>
2022-09-21 16:02:28 +0900

    chore: 개발환경 수정  - gitignore 및 prettier 수정

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,12 @@
 module.exports = {
-  bracketSpacing: false,
+  bracketSpacing: true,
   jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
   arrowParens: 'avoid',
+  printWidth: 150,
+  semi: false,
+  endOfLine: 'lf',
+  tabWidth: 2,
+  useTabs: false,
 };
